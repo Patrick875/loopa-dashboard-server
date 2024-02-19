c49e07d3a1c4645489944cb6879b334674623481
KUBWIMANA Patrick <52809972+Patrick875@users.noreply.github.com>
2024-02-19 11:58:46 +0200

    reducing the string

--- app.js
+++ app.js
@@ -10,10 +10,9 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({ origin: "*" }));
-// app.use(cors({ origin: "https://genuine-arithmetic-b0c8ef.netlify.app/" }));
 app.options("*", cors());
 
-app.use("/api/v1/", userRoutes);
-app.use("/api/v1/plots", plotRoutes);
+app.use("/", userRoutes);
+app.use("/plots", plotRoutes);
 
 module.exports = app;
